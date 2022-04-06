1b9909c73d5f163bb90c6b13d75fb4aefd5184cd
YeXia <yexianningyue@126.com>
2022-04-06 18:53:54 +0800

    nothing

--- vs_taxAnno.v2.sh
+++ vs_taxAnno.v2.sh
@@ -1,1 +1,19 @@
-/share/data2/guorc/script/WGS/Virus/vs_taxAnno.v2.sh
+
+#!/bin/bash
+
+if [ "$#" -ne "2" ]; then
+    echo -e "\nusage: sh $0 <AA faa> <output file>\n" 
+    exit 2
+fi
+
+
+prot=$1
+outf=$2
+
+diamond blastp --threads 60 --max-target-seqs 10 --db /share/data2/guorc/Database/virus/20220331/tot.faa --query $prot --outfmt 6 --out $outf.bt --quiet 
+perl /share/data2/guorc/script/get_pep.length.pl $prot $outf.len
+filter_blast -i $outf.bt -o $outf.bt2 --qfile $outf.len --qper 50 --identity 30 --tops 40 --score 50 
+grep '^>' $prot | sed 's/^>//' |perl -pne 's/_(\d+) #.*//' |sort |uniq -c |awk '{print $2"\t"$1}' > $outf.ngenes
+perl /share/data2/guorc/script/WGS/Virus/vctg_stat.v2.pl $outf.bt2 /share/data2/guorc/Database/virus/20220331/tot.tax $outf.ngenes $outf.bt2.f
+awk '$2!="NA"' $outf.bt2.f > $outf.bt2.f2
+msort -k 1,rn3,rn6 $outf.bt2.f2 | perl -ne 'chomp;@s=split /\s+/;next if exists $h{$s[0]}; $pct=$s[2]/$s[4]; next unless $pct>=0.2 and $s[5]>=30; printf "$s[0]\t$s[2]/$s[4]\t%.2f\t$s[1]\n",$s[5];$h{$s[0]}=1;' > $outf.tax_family
